9036afd6b9004e19e5e3ae9c4f209d701b49126d
konstantinGulish <konstantin_gulish@yahoo.com>
2018-06-12 10:13:32 -0400

    Initial commit

---
+++ src/Division.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Division {
+        public static void main(String[] args) {
+
+           try {
+               int a, b, result;
+
+               Scanner input = new Scanner(System.in);
+               System.out.println("Input two integers");
+
+               a = input.nextInt();
+               b = input.nextInt();
+
+               result = a / b;
+
+               System.out.println("Result = " + result);
+           }
+
+           catch (ArithmeticException e){
+               System.out.println("You can't divide by 0. " +
+               "Please re-run the program.");
+           }
+        }
+}
